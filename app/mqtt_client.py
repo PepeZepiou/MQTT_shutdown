439c01a0a3247178222f0fc1087780f4fe84fb2d
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-05-30 15:49:07 +0200

    Refactor MQTT client by removing hardcoded values

--- app/mqtt_client.py
+++ app/mqtt_client.py
@@ -2,67 +2,13 @@ import json
 #import subprocess
 
 import paho.mqtt.client as mqtt
+from config.config import *
 
-
-BROKER = "192.168.10.11"
-PORT = 1883
-
-USERNAME = "mqtt_user"
-PASSWORD = "********"
-
-DEVICE_ID = "ftp_server"
-
-TOPIC_CMD_SHUTDOWN = "ftp_server/cmd/shutdown"
-
-TOPIC_ONLINE = "ftp_server/status/online"
-TOPIC_FTP_ACTIVE = "ftp_server/status/ftp_active"
-TOPIC_SYNCING = "ftp_server/status/syncing"
-TOPIC_STATE = "ftp_server/status/state"
-
-DISCOVERY_TOPIC = f"homeassistant/device/{DEVICE_ID}/config"
 
 # Device Discovery Message
 def publish_discovery(client):
 
-    payload = {
-        "dev": {
-            "ids": DEVICE_ID,
-            "name": "FTP Server",
-            "mf": "Homelab",
-            "mdl": "Arch Linux FTP"
-        },
-
-        "o": {
-            "name": "mqtt-ftp-client"
-        },
-
-        "cmps": {
-
-            "online": {
-                "p": "binary_sensor",
-                "device_class": "connectivity",
-                "value_template": "{{ value }}",
-                "state_topic": TOPIC_ONLINE
-            },
-
-            "ftp_active": {
-                "p": "binary_sensor",
-                "value_template": "{{ value }}",
-                "state_topic": TOPIC_FTP_ACTIVE
-            },
-
-            "syncing": {
-                "p": "binary_sensor",
-                "value_template": "{{ value }}",
-                "state_topic": TOPIC_SYNCING
-            },
-
-            "state": {
-                "p": "sensor",
-                "state_topic": TOPIC_STATE
-            }
-        }
-    }
+    payload = DEVICE_DISCOVERY_PAYLOAD
 
     client.publish(
         DISCOVERY_TOPIC,
@@ -87,41 +33,7 @@ def on_message(client, userdata, msg):
         )
 
         shutdown_sequence(client)
-
-
-def shutdown_sequence(client):
-
-    client.publish(
-        TOPIC_SYNCING,
-        "true",
-        retain=True
-    )
-
-    client.publish(
-        TOPIC_STATE,
-        "syncing",
-        retain=True
-    )
-
-    # Exemple :
-    # subprocess.run(["rsync", ...])
-
-    time.sleep(5)
-
-    client.publish(
-        TOPIC_SYNCING,
-        "false",
-        retain=True
-    )
-
-    client.publish(
-        TOPIC_STATE,
-        "shut_down",
-        retain=True
-    )
-
-    # subprocess.run(["shutdown", "-h", "now"])
-
+        
 
 # MQTT Initialization
 client = mqtt.Client(
@@ -151,23 +63,7 @@ client.connect(
 
 publish_discovery(client)
 
-client.publish(
-    TOPIC_ONLINE,
-    "true",
-    retain=True
-)
-
-client.publish(
-    TOPIC_FTP_ACTIVE,
-    "true",
-    retain=True
-)
-
-client.publish(
-    TOPIC_STATE,
-    "running",
-    retain=True
-)
+publish_state(client)
 
 client.subscribe(
     TOPIC_CMD_SHUTDOWN
