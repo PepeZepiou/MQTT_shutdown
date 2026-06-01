5828c3ca40377db81350d7392fb46de7e75e5952
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-06-02 01:48:07 +0200

    Clean up config.py by removing unused code
    Removed unused imports and commented-out JSON payload for MQTT integration.

--- app/config/config.py
+++ app/config/config.py
@@ -1,7 +1,3 @@
-import time
-import os
-import subprocess
-
 # BROKER INFORMATIONS
 BROKER = os.getenv("MQTT_ADDRESS", "192.168.1.11")
 PORT = int(os.getenv("MQTT_PORT", "1883"))
@@ -27,69 +23,3 @@ TOPIC_EVENT = f"mqtt2shutdown/{DEVICE_ID}/event/event"
 TOPIC_ERROR = f"mqtt2shutdown/{DEVICE_ID}/event/error"        # THIS SHOULD BE USED LATER IN OT LIKE DIY SENSORS
 # HOME ASSISTANT AUTO-DISCOVERY TOPIC
 DISCOVERY_TOPIC = f"homeassistant/device/{DEVICE_ID}/config"
-
-# JSON PAYLOAD FOR MQTT INTEGRATION AUTO-DISCOVERY
-# NOTES : ERROR AND UPTIME WILL NOT BE USED FOR THE MOMENT
-# I HAVE TO FIND A WAY TO CHEK FOR ERRORS
-# AND I HAVE TO CHECK HOW TO REPRESENT TIME IN MQTT INTEGRATION
-DEVICE_DISCOVERY_PAYLOAD = {
-        "dev": {
-            "ids": DEVICE_ID,
-            "name": DEVICE_NAME,
-            "mf": DEVICE_MANUFACTURER,
-            "mdl": DEVICE_MODEL
-        },
-        "o": {
-            "name": "mqtt2shutdown"
-        },
-        "cmps": {
-            "online": {
-                "p": "binary_sensor",
-                "name": "Connectivity",
-                "device_class": "connectivity",
-                "value_template": "{{ value }}",
-                "state_topic": TOPIC_ONLINE,
-                "unique_id": f"{DEVICE_ID}_connectivity_sensor"
-            },
-            "state": {
-                "p": "sensor",
-                "name": "State",
-                "state_topic": TOPIC_STATE,
-                "unique_id": f"{DEVICE_ID}_state_sensor"
-            },
-            "event": {
-                "p": "sensor",
-                "name": "Event",
-                "state_topic": TOPIC_EVENT,
-                "unique_id": f"{DEVICE_ID}_event_sensor"
-            },   
-            "shutdown": {
-                "p": "button",
-                "name": "Shutdown Button",
-                "command_topic": TOPIC_CMD_SHUTDOWN,
-                "unique_id": f"{DEVICE_ID}_cmd_button"
-            },
-            "heartbeat": {
-                "p": "sensor",
-                "device_class": "timestamp",
-                "name": "HeartBeat",
-                "value_template": "{{ as_datetime(value) }}",
-                "unique_id": f"{DEVICE_ID}_heartbeat_sensor",
-                "state_topic": TOPIC_HEARTBEAT
-            },
-            "error": {
-                "p": "sensor",
-                "name": "Error",
-                "state_topic": TOPIC_ERROR,
-                "unique_id": f"{DEVICE_ID}_error_sensor"
-            },
-            "uptime": {
-                "p": "sensor",
-                "name": "Uptime",
-                "device_class": "timestamp",
-                "value_template": "{{ as_datetime(value) }}",
-                "unique_id": f"{DEVICE_ID}_uptime_sensor",
-                "state_topic": TOPIC_UPTIME
-            }
-        }
-    }
