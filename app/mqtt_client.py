660083e186a907280e24e36362c46f73022e74d3
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-06-02 02:20:17 +0200

    Refactor MQTT client to use settings module

--- app/mqtt_client.py
+++ app/mqtt_client.py
@@ -1,34 +1,23 @@
 import paho.mqtt.client as mqtt
-import config.config as config
-import config.modules as modules
-import time
+import config.settings as settings
+from modules.main_func import main
+from modules.basic_func import on_message, on_connect
         
 
 # MQTT Initialization
-client = mqtt.Client(
-    mqtt.CallbackAPIVersion.VERSION2
-)
-client.username_pw_set(
-    config.USERNAME,
-    config.PASSWORD
-)
-client.will_set(
-    config.TOPIC_ONLINE,
-    payload="OFF",
-    qos=1,
-    retain=True
-)
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.username_pw_set(settings.USERNAME, settings.PASSWORD)
+client.will_set(settings.TOPIC_ONLINE, payload="OFF", qos=1, retain=True)
 client.reconnect_delay_set(min_delay=5, max_delay=60)
-client.on_message = modules.on_message
-client.on_connect = modules.on_connect
+client.on_message = on_message
+client.on_connect = on_connect
 
-client.connect(
-    config.BROKER,
-    config.PORT,
-    60
-)
+# MQTT Connexion
+client.connect(settings.BROKER, settings.PORT, 60)
 
+# Start MQTT Thread
 client.loop_start()
 
-modules.main(client)
+# Start main loop
+main(client)
 
