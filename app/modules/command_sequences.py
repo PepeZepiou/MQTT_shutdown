7bb6639496620d86a47a829f60584be3b9de891b
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-06-02 02:00:45 +0200

    Change config import to settings in command_sequences.py

--- app/modules/command_sequences.py
+++ app/modules/command_sequences.py
@@ -1,9 +1,9 @@
-import config.config as config
+import config.settings as settings
 import subprocess
 
 def shutdown_sequence(client):
-    client.publish(config.TOPIC_STATE, "shutting_down", retain=True)
-    client.publish(config.TOPIC_EVENT, "shutting_down_requested", retain=False)
+    client.publish(settings.TOPIC_STATE, "shutting_down", retain=True)
+    client.publish(settings.TOPIC_EVENT, "shutting_down_requested", retain=False)
     # Command used in container
     #with open("/shutdown/request", "w") as f:
     #    f.write("shutdown")
