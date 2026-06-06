82ec73399ed30f781896b4310321af7cafbf0913
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-06-06 10:46:46 +0200

    Change shutdown request file path in command_sequences.py
    Updated the shutdown request file path for container usage.

--- app/modules/command_sequences.py
+++ app/modules/command_sequences.py
@@ -5,7 +5,7 @@ def shutdown_sequence(client):
     client.publish(settings.TOPIC_STATE, "shutting_down", retain=True)
     client.publish(settings.TOPIC_EVENT, "shutting_down_requested", retain=False)
     # Command used in container
-    #with open("/shutdown/request", "w") as f:
+    #with open("/shared/commands/shutdown.request", "w") as f:
     #    f.write("shutdown")
     # Command used in dev with python
     subprocess.run(["shutdown", "-h", "now"])
