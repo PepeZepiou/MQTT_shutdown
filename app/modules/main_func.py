b87075834cfbc46a7f990465d3633b99c5856fdd
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-06-07 06:05:57 +0200

    Change heartbeat interval and error publishing behavior
    Updated heartbeat interval to 30 seconds and modified error publishing to not retain messages.

--- app/modules/main_func.py
+++ app/modules/main_func.py
@@ -1,17 +1,20 @@
 import modules.basic_func as func
 import time
 
-
+# The main function is a loop.
+# I choose this architecture to avoid to interupt paho thread with a sleep used for heartbeat publication.
+# Could integrate anything later, but for the moment just publish the heartbeat each 30s 
+# and publish on error topic in case of.
 
 def main(client):
     last_heartbeat = 0
     while True:
         try:
             now = time.time()
-            if now - last_heartbeat > 10:
+            if now - last_heartbeat > 30:
                 func.publish_heartbeat(client)
                 last_heartbeat = now
         except Exception as e:
-            func.publish_error(client, f"Error in main loop: {e}")
+            func.publish_error(client, f"Error in main loop: {e}", retain=False)
 
         time.sleep(1)
