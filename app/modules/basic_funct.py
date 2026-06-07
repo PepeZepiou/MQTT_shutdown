c85f29b5592740e91055c283c33fa19719df9b0e
PepeZepiou <89185688+PepeZepiou@users.noreply.github.com>
2026-06-07 05:32:15 +0200

    Enhance publish_state with uptime and error reporting
    Refactor publish_state function to include uptime and error topics.

--- app/modules/basic_funct.py
+++ app/modules/basic_funct.py
@@ -13,13 +13,9 @@ def heartbeat_loop(client):
 
 
 
-def publish_state(client):
-    client.publish(settings.TOPIC_ONLINE, "ON", retain=True)
-    client.publish(settings.TOPIC_STATE, "running", retain=True)
-    client.publish(settings.TOPIC_EVENT, "state published", retain=False)
-
-
-
+# Function used at connexion to mqtt broker.
+# Send the discovery message which will configure device on Home ASSISTANT
+# And send this activity as an event.
 def publish_discovery(client):
     payload = settings.DEVICE_DISCOVERY_PAYLOAD
     client.publish(settings.DISCOVERY_TOPIC, json.dumps(payload), retain=True)
@@ -27,16 +23,32 @@ def publish_discovery(client):
 
 
 
+# Function used just at connexion to mqtt broker, just after publication of discovery message.
+# Publish normal initials state.
+def publish_state(client):
+    client.publish(settings.TOPIC_ONLINE, "ON", retain=True)
+    client.publish(settings.TOPIC_UPTIME, datetime.now(timezone.utc).isoformat(), retain=True) 
+    client.publish(settings.TOPIC_STATE, "running", retain=True)
+    client.publish(settings.TOPIC_EVENT, "state published", retain=False)
+    client.publish(settings.TOPIC_ERROR, "none", retain=False)
+
+
+
+# Function used in the main loop.
+# Idea is to publish an heartbeat. With this, it's possible to raise an alarm in Home assistant if broker shutdown.
 def publish_heartbeat(client):
     client.publish(settings.TOPIC_HEARTBEAT, str(int(time.time())), retain=False)
 
 
-
+# Function used in main loop.
+# Publish on error topic if an error is catched by "try:/except:"
 def publish_error(client, msg):
     client.publish(settings.TOPIC_ERROR, msg, retain=False)
-    
 
 
+
+# Usual callback for paho thread.
+# At connection, send discovery message, publish state and suscribe to command topic
 def on_connect(client, userdata, flags, reason_code, properties):
     publish_discovery(client)
     publish_state(client)
@@ -44,6 +56,9 @@ def on_connect(client, userdata, flags, reason_code, properties):
 
 
 
+# Usual callback for paho thread.
+# For the moment, any message published on command topic initiate shutdown sequence.
+# In future, could initiate restart device, restart machine, acknowledge error message...
 def on_message(client, userdata, msg):
     topic = msg.topic
     payload = msg.payload.decode()
